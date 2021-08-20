cede2f0e000a8569a76c83f0616b1df49542864c
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-08-20 07:07:26 -0400

    The Basics: Type annotations

---
+++ annotations/variables.ts
@@ -0,0 +1,38 @@
+// the colon and the type "number" is the type annotation
+// we will only assign the value of number to the variable of apples
+const apples: number = 5;
+
+// this works for "let" too
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+
+// built in objects
+let now: Date = new Date()
+
+// Array
+let colors: string[] = ['red', 'green', 'blue']
+
+let myNumbers: number[] = [1,2,3]
+
+// Classes
+class Car {
+
+}
+// the var car is only ever going to refer to an instance of Car
+let car: Car = new Car()
+
+// Object literal
+let point: { x: number; y: number } = {
+    x:10,
+    y:20
+}
+
+// Function
+// the annotation is ' (i: number) => void '
+// this annotation tells ts what type of value we want to assign to the variable 'logNumber'
+// Everything on the rt side of ' = ' is the function
+const logNumber: (i: number) => void = (i: number) => {
+    console.log(i)
+}
