3b8eed1c8f587249ee3197e91f262893f14928c1
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-08-22 07:38:07 -0400

    Initial project with Faker data for User and Company

---
+++ maps/src/index.ts
@@ -0,0 +1,10 @@
+import { User } from './User'
+import { Company } from './Company'
+
+const user = new User()
+
+console.log(user)
+// console.log('test')
+
+const company = new Company()
+console.log(company)
