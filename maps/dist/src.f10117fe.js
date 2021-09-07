aa61c1c75761bc98bd69278747242caca770bc06
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-09-07 05:51:57 -0400

    Created a popup window to open when a user clicks the marker

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -136947,12 +136947,20 @@ var CustomMap = function () {
   }
 
   CustomMap.prototype.addMarker = function (mappable) {
-    new google.maps.Marker({
+    var _this = this;
+
+    var marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    });
+    marker.addListener('click', function () {
+      var infoWindow = new google.maps.InfoWindow({
+        content: 'Hi there!'
+      });
+      infoWindow.open(_this.googleMap, marker);
     });
   };
 
