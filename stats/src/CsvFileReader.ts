b93c0f8721b6fb5b1fe399ffe0ae4d596605b84d
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-09-13 05:47:16 -0400

    Moving out the mapRow functionality specific to the football data to make the csvreader reuseable again.

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
 
-type MatchData = [Date, string, string, number, number, MatchResult, string]
+type MatchData = [Date, string, string, number, number, MatchResult, string];
 
 export class CsvFileReader {
 	data: MatchData[] = [];
@@ -15,21 +15,21 @@ export class CsvFileReader {
 				encoding: 'utf-8',
 			})
 			.split('\n')
-			.map(
-                (row: string): string[] => {
-				    return row.split(',');
-			    }
-            )
-            .map((row: string[]): MatchData => {
-                return [
-                    dateStringToDate(row[0]),
-                    row[1],
-                    row[2],
-                    parseInt(row[3]),
-                    parseInt(row[4]),
-                    row[5] as MatchResult,
-                    row[6]
-                ]
-            })
+			.map((row: string): string[] => {
+				return row.split(',');
+			})
+			.map(this.mapRow);
 	}
+
+    mapRow(row: string[]): MatchData {
+        return [
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult,
+            row[6],
+        ];
+    }
 }
