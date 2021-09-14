4a26861305cb04bfdbf4a290ab3b6bc2e9f18be5
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-09-14 05:51:11 -0400

    Moving MatchData to its own file for easier reuse and reading into MatchReader

--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -1,7 +1,6 @@
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
-
-type MatchData = [Date, string, string, number, number, MatchResult, string]
+import { MatchData } from './MatchData';
 
 interface DataReader {
 	read(): void;
@@ -9,17 +8,22 @@ interface DataReader {
 }
 
 export class MatchReader {
+	matches: MatchData[] = [];
+
 	constructor(public reader: DataReader) {}
 
-    // .map((row: string[]): MatchData => {
-    //     return [
-    //         dateStringToDate(row[0]),
-    //         row[1],
-    //         row[2],
-    //         parseInt(row[3]),
-    //         parseInt(row[4]),
-    //         row[5] as MatchResult,
-    //         row[6]
-    //     ]
-    // })
+	load(): void {
+		this.reader.read();
+		this.matches = this.reader.data.map((row: string[]): MatchData => {
+			return [
+				dateStringToDate(row[0]),
+				row[1],
+				row[2],
+				parseInt(row[3]),
+				parseInt(row[4]),
+				row[5] as MatchResult,
+				row[6],
+			];
+		});
+	}
 }
