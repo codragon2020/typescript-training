4e36820a5eb3ef7b3f7f41c7f752ae21c4cd850d
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-09-14 06:11:50 -0400

    Created simple ConsoleReport class for the Summary class and object composition implementation. Updated Index to use entire object composition methodology

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,6 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
 import { CsvFileReader } from './CsvFileReader';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
 // Create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv');
@@ -10,6 +12,9 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
+const summary = new Summary(
+    new WinsAnalysis('Man United'),
+    new ConsoleReport()
+)
 
-
-console.log(`Man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches)
