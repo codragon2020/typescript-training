fc145e2b70ccc39c6d7845042d0cc8b262b3890c
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-09-14 06:33:33 -0400

    Add static method for csvfilereader and implemented within index.  Cleaned up index

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,13 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
-var CsvFileReader_1 = require("./CsvFileReader");
 var Summary_1 = require("./Summary");
-// Create an object that satisfies the 'DataReader' interface
-var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
-// Create an instance of MatchReader and pass in something that
-// satisfies the 'DataReader' interface
-var matchReader = new MatchReader_1.MatchReader(csvFileReader);
-matchReader.load();
+var matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
 var summary = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
