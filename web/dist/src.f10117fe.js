52f8b9b75bfab20927d55fcb629ecb90e5df056f
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-09-15 06:23:22 -0400

    Added trigger function with handlers event to trigger forEach callback. It will return early if no handlers exist

--- web/dist/src.f10117fe.js
+++ web/dist/src.f10117fe.js
@@ -145,6 +145,18 @@ var User = function () {
     this.events[eventName] = handlers;
   };
 
+  User.prototype.trigger = function (eventName) {
+    var handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(function (callback) {
+      callback();
+    });
+  };
+
   return User;
 }();
 
