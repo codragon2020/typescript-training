89ba501f75bd3ea866dfb79176794fb78bbc8418
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-09-16 06:24:52 -0400

    Created fetch and save functions to get, post, put records into db.json

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,15 +1,27 @@
+// import axios from 'axios'
+
+// axios.post('http://localhost:3000/users', {
+//     name: 'myname',
+//     age: 20
+// })
+
+// axios.delete('http://localhost:3000/users/1')
+
+
 import { User } from "./models/User";
 
-const user = new User({ name: 'myname', age: 20 })
+const user = new User({ name: 'no record', age: 10 })
 
-user.on('change', () => {
-    console.log('Change #1')
-})
-user.on('change', () => {
-    console.log('Change #2')
-})
-user.on('save', () => {
-    console.log('Save was triggered')
-})
+user.save()
 
-user.trigger('change')
+// user.on('change', () => {
+//     console.log('Change #1')
+// })
+// user.on('change', () => {
+//     console.log('Change #2')
+// })
+// user.on('save', () => {
+//     console.log('Save was triggered')
+// })
+
+// user.trigger('change')
