8936598ba85684dcfd866a6f3afefc40811f787d
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-09-18 06:24:06 -0400

    Extracting an Attributes class. Refactoring with Generic type

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,4 @@
-import { Eventing } from './Eventing'
+import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 
 export interface UserProps {
@@ -7,20 +7,9 @@ export interface UserProps {
 	age?: number;
 }
 
-const rootUrl = 'http://localhost:3000/users'
+const rootUrl = 'http://localhost:3000/users';
 
 export class User {
-	public events: Eventing = new Eventing()
-    public sync: Sync<UserProps> = new Sync<UserProps>()
-    
-    constructor(private data: UserProps) {}
-
-	get(propName: string): number | string {
-		return this.data[propName];
-	}
-
-	set(update: UserProps): void {
-		Object.assign(this.data, update);
-	}
-
+	public events: Eventing = new Eventing();
+	public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 }
