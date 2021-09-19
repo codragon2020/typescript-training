847dfc5958e272ad126ebae48c19450c69211d60
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-09-19 07:15:25 -0400

    Created set and fetch methods

--- web/dist/src.f10117fe.js
+++ web/dist/src.f10117fe.js
@@ -2389,6 +2389,26 @@ var User = function () {
     enumerable: false,
     configurable: true
   });
+
+  User.prototype.set = function (update) {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  };
+
+  User.prototype.fetch = function () {
+    var _this = this;
+
+    var id = this.attributes.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+
+    this.sync.fetch(id).then(function (response) {
+      _this.set(response.data);
+    });
+  };
+
   return User;
 }();
 
@@ -2403,14 +2423,12 @@ Object.defineProperty(exports, "__esModule", {
 var User_1 = require("./models/User");
 
 var user = new User_1.User({
-  name: 'new record',
-  age: 0
+  id: 1
 });
-console.log(user.get('name'));
 user.on('change', function () {
-  console.log('User was changed');
+  console.log(user);
 });
-user.trigger('change');
+user.fetch();
 },{"./models/User":"src/models/User.ts"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
