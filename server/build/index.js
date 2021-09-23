4442d83a7a805b0d4dba02fc3a844bb75d8cec48
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-09-23 06:26:20 -0400

    Wired up a POST route to see the body-parser in action. Body-parser is deprecated, so switched to express for body parsing

--- server/build/index.js
+++ server/build/index.js
@@ -5,7 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var loginRoutes_1 = require("./routes/loginRoutes");
+// import bodyParser from 'body-parser'
 var app = express_1.default();
+// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express_1.default.urlencoded({ extended: true }));
 app.use(loginRoutes_1.router);
 app.listen(3000, function () {
     console.log('Listening on port 3000');
